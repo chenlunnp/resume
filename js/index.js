8f8246634b1db8d405997eaf9c20902032935e20
Tomb <1023743013@qq.com>
2017-04-19 19:38:53 +0800

    s
    s

---
+++ js/index.js
@@ -0,0 +1,59 @@
+$(function(){
+
+	alert("既然你都进来了，何不跟我联系一下呢(>^ω^<)");
+	var sm_items  = $(".sm_item");
+	var stu_contents = $(".stu_content");
+
+//焦点轮播
+	sm_items.each(function(index) {
+		var itemNode = $(this);
+		itemNode.mouseover(function() {
+			itemNode.addClass('item_selected').siblings().removeClass('item_selected');
+			var index = itemNode.index();
+			stu_contents.eq(index).fadeIn(300).siblings().fadeOut(300);
+		});
+	});
+
+//自动轮播
+	var  i = 0;
+	var timer = setInterval(autoplay,3000);
+
+	function autoplay(){
+		i++;
+		if( i == 3){
+			i = 0;
+		};
+		// console.log(i);
+		stu_contents.eq(i).fadeIn(300).siblings().fadeOut(300);
+		sm_items.eq(i).addClass('item_selected').siblings().removeClass('item_selected');
+	};
+
+	$("#studyMessage").hover(function() {
+		clearInterval(timer);
+	}, function() {
+		timer = setInterval(autoplay,3000);
+	});
+
+//点击轮播
+	var prev = $("#btns .left_btn"),
+		next = $("#btns .right_btn");
+
+	prev.click(function(){
+		i--;
+		if(i == -1){
+			i = 2;
+		};
+		stu_contents.eq(i).fadeIn(300).siblings().fadeOut(300);
+		sm_items.eq(i).addClass('item_selected').siblings().removeClass('item_selected');
+	});
+
+	next.click(function(){
+		autoplay();
+	});
+//返回到顶部
+	var backTop = $("#backTop");
+	backTop.click(function() {
+		$("body,html").animate({scrollTop:0}, 500);
+	});
+});
+
